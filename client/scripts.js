a4e4c56887d1d6f1acd1c3c542767e29e61d361d
Taylor Wood <taylorwood39@gmail.com>
2020-09-03 20:45:41 -0500

    Add replace method to classList

---
+++ client/scripts.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('nav')
+
+document.querySelector('.fa-hamburger').addEventListener('click', () => {
+  nav.classList.replace('is-collapsed', 'is-expanded') ||
+  nav.classList.replace('is-expanded', 'is-collapsed')
+})
