27a5edaca52cac5e9bb6e25a52f85f147ad0103f
Taylor Wood <taylorwood39@gmail.com>
2020-09-10 20:50:30 -0500

    Add comment for personal notes

--- client/validation.mjs
+++ client/validation.mjs
@@ -1,7 +1,7 @@
 export const validate = () => {
   const currentInput = event.target;
   const regex = new RegExp(currentInput.pattern)
-
+  // IF regex doesn't pass and value doesn't exist
   if (!regex.test(currentInput.value) || !currentInput.value ) {
     currentInput.classList.replace("is-success", "is-error") ||
       currentInput.classList.add("is-error");
