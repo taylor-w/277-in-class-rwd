8618067706530b769a6f43a83a1d2030a81dc969
Taylor Wood <taylorwood39@gmail.com>
2020-09-10 19:50:58 -0500

    Add validation for form inputs

--- client/scripts.js
+++ client/scripts.js
@@ -1,6 +1,22 @@
+/*nav*/
 const nav = document.querySelector('nav')
 
 document.querySelector('.fa-hamburger').addEventListener('click', () => {
   nav.classList.replace('is-collapsed', 'is-expanded') ||
   nav.classList.replace('is-expanded', 'is-collapsed')
+})
+
+/*form*/
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input => {
+  input.addEventListener('blur', (event) => {
+    const currentInput = event.target
+
+    if (!currentInput.value) {
+      currentInput.classList.replace("is-success","is-error") || currentInput.classList.add("is-error")
+    } else {
+      currentInput.classList.replace("is-error", "is-success") || currentInput.classList.add("is-success")
+    }
+  })
 })
