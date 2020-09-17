f7b25e71ce9a20f85155434b07cf2665f5453511
Taylor Wood <taylorwood39@gmail.com>
2020-09-17 18:46:27 -0500

    Remove unused event in script.js

--- client/scripts.js
+++ client/scripts.js
@@ -1,18 +1,18 @@
-import {validate} from "./validation.mjs"
+import { validate } from "./validation.mjs";
 
 /*nav*/
-const nav = document.querySelector('nav')
+const nav = document.querySelector("nav");
 
-document.querySelector('.fa-hamburger').addEventListener('click', () => {
-  nav.classList.replace('is-collapsed', 'is-expanded') ||
-  nav.classList.replace('is-expanded', 'is-collapsed')
-})
+document.querySelector(".fa-hamburger").addEventListener("click", () => {
+  nav.classList.replace("is-collapsed", "is-expanded") ||
+    nav.classList.replace("is-expanded", "is-collapsed");
+});
 
 /*form*/
-const inputs = document.querySelectorAll('input')
+const inputs = document.querySelectorAll("input");
 
-inputs.forEach(input => {
-  input.addEventListener('blur', (event) => {
+inputs.forEach((input) => {
+  input.addEventListener("blur", () => {
     validate();
-  })
-})
+  });
+});
