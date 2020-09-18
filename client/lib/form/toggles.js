a20a9ef871b2a80ee4e9f172cfa8b091433db2eb
Taylor Wood <taylorwood39@gmail.com>
2020-09-17 20:53:04 -0500

    Expand toggles script to filter 'regularFields' vs 'fields'

--- client/lib/form/toggles.js
+++ client/lib/form/toggles.js
@@ -3,6 +3,14 @@ const form = document.querySelector("form");
 // grab all divs except the last one
 const fields = form.querySelectorAll("div:not(:last-of-type)");
 
+// We need to separate out the 'special' from the 'regular' fields
+// TODO: Write a filter that will return out anything that doesn't have a 'data-status' attribute (HINT: hasAttribute("data-status"))
+
+const regularFields = [...fields].filter(
+  (field) => !field.hasAttribute("data-status")
+);
+
 export default () => {
   console.log(fields);
+  console.log(regularFields);
 };
